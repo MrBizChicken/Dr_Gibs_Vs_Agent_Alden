8347fd0c37f584a1a6be2ec96547c5d50446f209
MrBizChicken88 <mrbizchicken88@Users-MacBook-Air-2.local>
2022-06-06 20:02:56 -0500

    made states

--- main.py
+++ main.py
@@ -21,28 +21,13 @@ def main():
                 if event.key == pygame.K_q:
                     pygame.quit()
 
-            if pygame.sprite.groupcollide(make_level.player_group, make_level.door_group, False, False):
-
-                level = level + 1
-                print(level)
-                make_level.load_level(level)
 
 
         make_level.draw()
+        make_level.events()
         make_level.update()
 
-def load_level(self, level):
-    make_level.player_group.empty()
-    make_level.bullet_group.empty()
-    make_level.ammo_pickup_group.empty()
-    make_level.block_group.empty()
-    make_level.enemy_group.empty()
-    make_level.crate_group.empty()
-    make_level.gun_crate_group.empty()
-    make_level.gun2_pickup_group.empty()
-    make_level.solid_objects_group.empty()
-    make_level.door_group.empty()
-    make_level.load_level(level)
+
 
 
     pygame.quit()
--- make_level.py
+++ make_level.py
@@ -27,26 +27,72 @@ class Make_levels(pygame.sprite.Sprite):
         self.solid_objects_group = pygame.sprite.Group()
         self.door_group = pygame.sprite.Group()
 
-        self.filename = "LEVEL1.csv"
         self.screen_size = pygame.FULLSCREEN
         self.surface = pygame.display.set_mode((0, 0), self.screen_size)
         self.map = []
         self.level = ["LEVEL1.csv", "LEVEL2.csv", "LEVEL3.csv"]
-        self.level_num = 1
-
-        self.load_level(0)
-
-    # def change_level(self):
-    #     if self.level == 2:
-    #         self.filename = "LEVEL2.csv"
+        self.level_num = 0
+        self.states = ["start", "running", "paused"]
+        self.state = self.states[0]
+        self.running = True
+        self.load_level(self.level_num)
 
 
 
 
+    def events(self):
 
+        events = pygame.event.get()
+
+        for event in events:
+            # if event.type == pygame.QUIT:
+            #     self.running = False
+
+            #Keyboard
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+
+                #used to kill outside loop
+                if event.key == pygame.K_q:
+                    return True
+
+    
+
+
+                if event.key == pygame.K_p:
+
+                    if self.state == "paused" and event.key == pygame.K_p:
+                        self.state = "running"
+                    else:
+                        self.state = "paused"
+
+
+                if event.key == pygame.K_l:#SPACE
+                    if self.state == "start":
+                        self.state = "running"
+                        print("start")
+
+                if pygame.sprite.groupcollide(self.player_group, self.door_group, False, False):
+
+                    self.level_num = self.level_num + 1
+                    self.clear_level()
+
+
+    def clear_level(self):
+        self.player_group.empty()
+        self.bullet_group.empty()
+        self.ammo_pickup_group.empty()
+        self.block_group.empty()
+        self.enemy_group.empty()
+        self.crate_group.empty()
+        self.gun_crate_group.empty()
+        self.gun2_pickup_group.empty()
+        self.solid_objects_group.empty()
+        self.door_group.empty()
+        self.load_level(self.level_num)
     def get_list(self, level):
         map_list = ""
-        # reads the file and splits every new line into a list
+
         with open(level) as file:
             map_list = file.read().split("\n")
         # filter out all "#" and empty strings
@@ -108,98 +154,63 @@ class Make_levels(pygame.sprite.Sprite):
 
 
 
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    # def draw_level(self):
-    #
-    #
-    #     with open(self.filename, 'r') as csvfile:
-    #         reader = csv.reader(csvfile)
-    #         for level in reader:
-    #             self.map.append(level)
-    #
-    #     for row in range(len(self.map)):
-    #         for col in range(len(self.map[row])):
-    #
-    #
-    #             if self.map[row][col] == "s":
-    #                 self.block_group.add(stone.Stone(col * BLOCK_SIZE, row * BLOCK_SIZE))
-    #
-    #
-    #             if self.map[row][col] == "m":
-    #                 self.block_group.add(metal.Metal(col * BLOCK_SIZE, row * BLOCK_SIZE))
-    #
-    #             if self.map[row][col] == "e":
-    #                 self.enemy_group.add(henchmen.Henchmen(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-    #
-    #             if self.map[row][col] == "p":
-    #                 self.player_group.add(player.Player(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-    #
-    #             if self.map[row][col] == "c":
-    #                 self.crate_group.add(crate.Crate(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-    #
-    #             if self.map[row][col] == "g":
-    #                 self.gun_crate_group.add(gun_crate.Gun_crate(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-    #
-    #             if self.map[row][col] == "d":
-    #                 self.door_group.add(door.Door(col * BLOCK_SIZE, row * BLOCK_SIZE))
-    #
-    #
-    #
-    #             col += 1
-    #
-    #         row += 1
-    #
-
-
 
 
     def draw(self):
         self.surface.fill((20, 20, 20))
 
-        self.player_group.draw(self.surface)
 
-        self.ammo_pickup_group.draw(self.surface)
 
-        self.gun2_pickup_group.draw(self.surface)
 
-        self.bullet_group.draw(self.surface)
 
-        self.block_group.draw(self.surface)
 
-        self.enemy_group.draw(self.surface)
+        if self.state == "start":
+            self.surface.fill((100, 100, 255))#background
 
-        self.solid_objects_group.draw(self.surface)
+        elif self.state == "running":
 
-        self.door_group.draw(self.surface)
+
+            self.player_group.draw(self.surface)
+
+            self.ammo_pickup_group.draw(self.surface)
+
+            self.gun2_pickup_group.draw(self.surface)
+
+            self.bullet_group.draw(self.surface)
+
+            self.block_group.draw(self.surface)
+
+            self.enemy_group.draw(self.surface)
+
+            self.solid_objects_group.draw(self.surface)
+
+            self.door_group.draw(self.surface)
+
+        elif self.state == "paused":
+            self.surface.fill((255, 100, 100))#background
+
 
         pygame.display.flip()
 
 
-
-
     def update(self):
-        self.solid_objects_group.add(self.block_group , self.enemy_group, self.crate_group, self.gun_crate_group)
 
-        self.solid_objects_group.update(self.solid_objects_group, self.bullet_group, self.player_group)
+        if self.state == "start":
+            pass
 
-        self.crate_group.update(self.solid_objects_group, self.bullet_group, self.player_group)
 
-        self.gun_crate_group.update(self.solid_objects_group, self.player_group, self.bullet_group)
+        elif self.state == "running":
+            self.solid_objects_group.add(self.block_group , self.enemy_group, self.crate_group, self.gun_crate_group)
+            self.solid_objects_group.update(self.solid_objects_group, self.bullet_group, self.player_group)
+            self.crate_group.update(self.solid_objects_group, self.bullet_group, self.player_group)
+            self.gun_crate_group.update(self.solid_objects_group, self.player_group, self.bullet_group)
+            self.player_group.update(self.solid_objects_group, self.bullet_group, self.ammo_pickup_group, self.gun2_pickup_group, self.player_group)
+            self.block_group.update(self.solid_objects_group, self.bullet_group, self.player_group)
+            self.bullet_group.update(self.solid_objects_group, self.bullet_group, self.crate_group, self.ammo_pickup_group, self.enemy_group, self.gun_crate_group, self.gun2_pickup_group, self.player_group)
+            self.enemy_group.update(self.solid_objects_group, self.bullet_group, self.player_group)
+            self.ammo_pickup_group.update()
+            self.gun2_pickup_group.update()
 
-        self.player_group.update(self.solid_objects_group, self.bullet_group, self.ammo_pickup_group, self.gun2_pickup_group, self.player_group)
 
-        self.block_group.update(self.solid_objects_group, self.bullet_group, self.player_group)
-
-        self.bullet_group.update(self.solid_objects_group, self.bullet_group, self.crate_group, self.ammo_pickup_group, self.enemy_group, self.gun_crate_group, self.gun2_pickup_group, self.player_group)
-
-        self.enemy_group.update(self.solid_objects_group, self.bullet_group, self.player_group)
-
-        self.ammo_pickup_group.update()
-
-        self.gun2_pickup_group.update()
+        elif self.state == "paused":
+            pass
